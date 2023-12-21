bcc84f3c546b5dd3c7b23036913a0ed40f398119
Rocha <rodrigorochasantos@outlook.com>
2023-12-20 22:41:13 -0300

    Add description about the problem

--- algorithms-in-kotlin/src/main/kotlin/com/rochards/leetcode/arrays/Problem1920.kt
+++ algorithms-in-kotlin/src/main/kotlin/com/rochards/leetcode/arrays/Problem1920.kt
@@ -1,5 +1,12 @@
 package main.kotlin.com.rochards.leetcode.arrays
 
+/*
+* Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]]
+* for each 0 <= i < nums.length and return it.
+* A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+*
+* Full description to problem: https://leetcode.com/problems/build-array-from-permutation/description/
+* */
 fun buildArray(nums: IntArray): IntArray {
     val ans = IntArray(nums.size)
     for (i in nums.indices) {
