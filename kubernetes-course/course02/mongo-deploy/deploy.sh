f172e5660f8e9064198a4b766ee7f9e059fcdb67
Rocha <rodrigorochasantos@outlook.com>
2024-01-11 21:57:56 -0300

    Changes to kubernetes-course
    Changes in course02:
    - Add and delete lines in deploy.sh;
    - Add files mongodb-configmap.yaml and mongoexpress-deploy.yaml

--- kubernetes-course/course02/mongo-deploy/deploy.sh
+++ kubernetes-course/course02/mongo-deploy/deploy.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
 kubectl apply -f mongodb-secret.yaml
-if kubectl get secret mongodb-secret > /dev/null 2>&1; then
-    echo "Secret applied successfully."
-else
-    echo "Error applying Secret. Deployment aborted."
-    exit 1
-fi
+# if kubectl get secret mongodb-secret > /dev/null 2>&1; then
+#     echo "'mongodb-secret' applied successfully."
+# else
+#     echo "Error applying 'mongodb-secret'."
+#     exit 1
+# fi
 
 kubectl apply -f mongodb-deploy.yaml
-if kubectl get deployment mongodb-deployment > /dev/null 2>&1; then
-    echo "Deployment applied successfully."
-else
-    echo "Error applying Deployment."
-    exit 1
-fi
 
-if kubectl get service mongodb-service > /dev/null 2>&1; then
-    echo "Service applied successfully."
-else
-    echo "Error applying Service."
-fi
+kubectl apply -f mongodb-configmap.yaml
+
+kubectl apply -f mongoexpress-deploy.yaml
