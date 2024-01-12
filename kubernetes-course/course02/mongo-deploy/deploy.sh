cea4af739f4d13c72f8ee700e560a4aa42bcea14
Rocha <rodrigorochasantos@outlook.com>
2024-01-11 21:24:35 -0300

    Changes to kubernetes-course
    Changes in course02
    - Add service configuration in mongodb-deploy.yaml;
    - Add lines in deploy.sh file

--- kubernetes-course/course02/mongo-deploy/deploy.sh
+++ kubernetes-course/course02/mongo-deploy/deploy.sh
@@ -9,8 +9,15 @@ else
 fi
 
 kubectl apply -f mongodb-deploy.yaml
-if kubectl get deployment  mongodb-deployment > /dev/null 2>&1; then
+if kubectl get deployment mongodb-deployment > /dev/null 2>&1; then
     echo "Deployment applied successfully."
 else
     echo "Error applying Deployment."
+    exit 1
+fi
+
+if kubectl get service mongodb-service > /dev/null 2>&1; then
+    echo "Service applied successfully."
+else
+    echo "Error applying Service."
 fi
