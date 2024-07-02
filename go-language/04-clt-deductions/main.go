a5e0993cf7945208d0e060c3eba9d87dab5ffcbb
Rocha <rodrigorochasantos@outlook.com>
2024-07-01 21:19:26 -0300

    go-language:
    - update project04

--- go-language/04-clt-deductions/main.go
+++ go-language/04-clt-deductions/main.go
@@ -49,38 +49,89 @@ func formatSalaryInBRL(salary float64) string {
 
 func calculateINSSDeductions(salary float64) float64 {
 
-	if salary <= 0 {
-		// lançar erro aqui
-		return 0
-	}
-
 	if salary > FOURTH_CEILING_RATE.income {
 		// valor acima da quarta faixa:
 		// - acima de R$ 7.786,02
-		return (FOURTH_CEILING_RATE.income-THIRD_CEILING_RATE.income)*FOURTH_CEILING_RATE.contribution + calculateINSSDeductions(THIRD_CEILING_RATE.income)
+		diff := FOURTH_CEILING_RATE.income - THIRD_CEILING_RATE.income
+		partialContribution := diff * FOURTH_CEILING_RATE.contribution
 
-	} else if salary > THIRD_CEILING_RATE.income {
+		fmt.Printf("4ª faixa => R$ %s - R$ %s = R$ %s x %s = R$ %s\n",
+			formatSalaryInBRL(FOURTH_CEILING_RATE.income),
+			formatSalaryInBRL(THIRD_CEILING_RATE.income),
+			formatSalaryInBRL(diff),
+			formatSalaryInBRL(FOURTH_CEILING_RATE.contribution),
+			formatSalaryInBRL(partialContribution),
+		)
+		return partialContribution + calculateINSSDeductions(THIRD_CEILING_RATE.income)
+	}
+
+	if salary > THIRD_CEILING_RATE.income {
 		// valor acima do teto da terceira faixa, por tanto dentro da quarta faixa:
 		// - acima de R$ 4.000,03, até R$ 7786,02
-		return (salary-THIRD_CEILING_RATE.income)*FOURTH_CEILING_RATE.contribution + calculateINSSDeductions(THIRD_CEILING_RATE.income)
+		diff := salary - THIRD_CEILING_RATE.income
+		partialContribution := diff * FOURTH_CEILING_RATE.contribution
 
-	} else if salary > SECOND_CEILING_RATE.income {
+		fmt.Printf("4ª faixa => R$ %s - R$ %s = R$ %s x %s = R$ %s\n",
+			formatSalaryInBRL(salary),
+			formatSalaryInBRL(THIRD_CEILING_RATE.income),
+			formatSalaryInBRL(diff),
+			formatSalaryInBRL(FOURTH_CEILING_RATE.contribution),
+			formatSalaryInBRL(partialContribution),
+		)
+
+		return partialContribution + calculateINSSDeductions(THIRD_CEILING_RATE.income)
+	}
+
+	if salary > SECOND_CEILING_RATE.income {
 		// valor acima do teto da segunda faixa, por tanto dentro da terceira faixa:
 		// - acima de R$ 2.666,68, até R$ 4.000,03
-		return (salary-SECOND_CEILING_RATE.income)*THIRD_CEILING_RATE.contribution + calculateINSSDeductions(SECOND_CEILING_RATE.income)
+		diff := salary - SECOND_CEILING_RATE.income
+		partialContribution := diff * THIRD_CEILING_RATE.contribution
 
-	} else if salary > FIRST_CEILING_RATE.income {
+		fmt.Printf("3ª faixa => R$ %s - R$ %s = R$ %s x %s = R$ %s\n",
+			formatSalaryInBRL(salary),
+			formatSalaryInBRL(SECOND_CEILING_RATE.income),
+			formatSalaryInBRL(diff),
+			formatSalaryInBRL(THIRD_CEILING_RATE.contribution),
+			formatSalaryInBRL(partialContribution),
+		)
+
+		return partialContribution + calculateINSSDeductions(SECOND_CEILING_RATE.income)
+	}
+
+	if salary > FIRST_CEILING_RATE.income {
 		// valor acima do teto da primeira faixa, por tanto dentro da segunda faixa:
 		// - acima de R$ 1.412,00, até R$ 2.666,68
+		diff := salary - FIRST_CEILING_RATE.income
+		partialContribution := diff * SECOND_CEILING_RATE.contribution
 
-		return (salary-FIRST_CEILING_RATE.income)*SECOND_CEILING_RATE.contribution + calculateINSSDeductions(FIRST_CEILING_RATE.income)
+		fmt.Printf("2ª faixa => R$ %s - R$ %s = R$ %s x %s = R$ %s\n",
+			formatSalaryInBRL(salary),
+			formatSalaryInBRL(FIRST_CEILING_RATE.income),
+			formatSalaryInBRL(diff),
+			formatSalaryInBRL(SECOND_CEILING_RATE.contribution),
+			formatSalaryInBRL(partialContribution),
+		)
+
+		return partialContribution + calculateINSSDeductions(FIRST_CEILING_RATE.income)
 
 	} else {
 		// caso básico da função
 
 		// salário dentro da primeira faixa, usa-se o valor do teto para o cálculo:
 		// - até R$ 1.412,00
-		return FIRST_CEILING_RATE.income * FIRST_CEILING_RATE.contribution
+
+		partialContribution := FIRST_CEILING_RATE.income * FIRST_CEILING_RATE.contribution
+
+		fmt.Printf("1ª faixa => R$ %s - R$ %s = R$ %s x %s = R$ %s\n",
+			formatSalaryInBRL(FIRST_CEILING_RATE.income),
+			formatSalaryInBRL(0),
+			formatSalaryInBRL(FIRST_CEILING_RATE.income),
+			formatSalaryInBRL(FIRST_CEILING_RATE.contribution),
+			formatSalaryInBRL(partialContribution),
+		)
+
+		return partialContribution
 	}
 
 }
